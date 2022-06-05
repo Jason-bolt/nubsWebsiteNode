724a5b6bd589a08b0aa0c9701d06a44dec1b7cf5
Jason-bolt <appiatujason@gmail.com>
2022-06-05 20:27:49 +0000

    Connected to database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const pool = require("./database/connection");
 app.use(express.static(__dirname + "/public"));
 const publicRouter = require("./routes/public");
 
