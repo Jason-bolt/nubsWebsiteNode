e9e10e855a2eede8904ca450eec7112bdf8dc72c
Jason-bolt <appiatujason@gmail.com>
2022-06-05 19:48:23 +0000

    Started auth

--- routes/public.js
+++ routes/public.js
@@ -83,4 +83,12 @@ router.get("/contact", (req, res) => {
 	res.render("contact", { page: "contact" });
 });
 
+router.get("/login", (req, res) => {
+	res.render("login");
+});
+
+router.get("/register", (req, res) => {
+	res.render("register");
+});
+
 module.exports = router;
