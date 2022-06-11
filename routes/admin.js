835f64246ed28334c8ebbdda76de6e32366861d3
Jason-bolt <appiatujason@gmail.com>
2022-06-11 23:46:53 +0000

    All admin routes added

--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,20 @@ router.get("/weekly_activities", (req, res) => {
 	res.render("admin/weekly_activities");
 });
 
+router.get("/news-events", (req, res) => {
+	res.render("admin/news_events");
+});
+
 router.get("/testimonies", (req, res) => {
-	res.render("admin/testimony");
+	res.render("admin/testimonies");
+});
+
+router.get("/gallery", (req, res) => {
+	res.render("admin/gallery");
+});
+
+router.get("/executives", (req, res) => {
+	res.render("admin/executives");
 });
 
 module.exports = router;
