fb419c9ed66303677c9534dcea96dfd032ab6b3e
Jason-bolt <appiatujason@gmail.com>
2022-07-17 20:42:02 +0000

    Successfully added user to database, left with full validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const pool = require("../database/connection");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 exports.register = (req, res) => {
 	const { first_name, last_name, email, password, password_confirm } = req.body;
@@ -7,21 +9,52 @@ exports.register = (req, res) => {
 	pool.query(
 		"SELECT email FROM admins WHERE email = ?",
 		[email],
-		(error, result) => {
-			// Check for errors
-			if (error) {
-				console.log(error);
-			}
+		async (error, results) => {
+			try {
+				// Check for errors
+				if (error) {
+					console.log(error);
+				}
 
-			// Check number of entries in the result
-			if (results.length > 0) {
-				return res.render("register", {
-					message: "That email is already in use",
-				});
-			} else if (password !== password_confirm) {
-				return res.render("register", {
-					message: "Passwords do not match",
-				});
+				// Check number of entries in the result
+				if (results.length > 0) {
+					return res.render("register", {
+						message: "That email is already in use",
+					});
+					// Check if passwords match
+				} else if (password !== password_confirm) {
+					return res.render("register", {
+						message: "Passwords do not match",
+						success: false,
+					});
+				}
+
+				let hashedPassword = await bcrypt.hash(password, 8);
+				// console.log(hashedPassword);
+
+				// Insert values in the database
+				pool.query(
+					"INSERT INTO admins SET ?",
+					{
+						first_name: first_name,
+						last_name: last_name,
+						email: email,
+						password: hashedPassword,
+					},
+					(error, results) => {
+						if (error) {
+							console.log(error);
+						} else {
+							console.log(results);
+							return res.render("register", {
+								message: "User registered",
+								success: true,
+							});
+						}
+					}
+				);
+			} catch (error) {
+				console.log(error);
 			}
 		}
 	);
--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/register", (req, res) => {
-	res.render("register");
+	res.render("register", { message: null, success: null });
 });
 
 router.get("/forgot_password", (req, res) => {
