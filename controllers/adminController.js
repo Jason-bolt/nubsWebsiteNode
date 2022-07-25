7e8518e9630c431f11cbe550fca39577428daf8a
Jason-bolt <appiatujason@gmail.com>
2022-07-25 15:17:26 +0000

    Added local data to new_events admin page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -42,10 +42,44 @@ exports.weekly_activities = (req, res) => {
 
 // Function to be returned when News/Events tab is clicked
 exports.news = (req, res) => {
+	const events = [
+		{
+			id: 1,
+			image: "carousel2.jpg",
+			name: "Event1",
+			details:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vel     quod mollitia vero blanditiis quaerat laborum ad in commodi veniam obcaecati dignissimos expedita sint, deserunt id sapiente qui est Amet.",
+		},
+		{
+			id: 2,
+			image: "carousel3.jpg",
+			name: "Event2",
+			details:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vel     quod mollitia vero blanditiis quaerat laborum ad in commodi veniam obcaecati dignissimos expedita sint, deserunt id sapiente qui est Amet.",
+		},
+		{
+			id: 3,
+			image: "carousel4.jpg",
+			name: "Event3",
+			details:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vel     quod mollitia vero blanditiis quaerat laborum ad in commodi veniam obcaecati dignissimos expedita sint, deserunt id sapiente qui est Amet.",
+		},
+		{
+			id: 4,
+			image: "carousel1.jpg",
+			name: "Event4",
+			details:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vel     quod mollitia vero blanditiis quaerat laborum ad in commodi veniam obcaecati dignissimos expedita sint, deserunt id sapiente qui est Amet.",
+		},
+	];
+
 	if (req.session.admin_id == undefined) {
 		res.redirect("/login");
 	} else {
-		res.render("admin/news_events", { page: "news" });
+		res.render("admin/news_events", {
+			page: "news",
+			events: events,
+		});
 	}
 };
 
