b89498651c3a0016e9e48f9f96bbf52cf0799ecb
Jason-bolt <appiatujason@gmail.com>
2022-07-25 07:35:38 +0000

    Local data used for admin weekly activities page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,10 +1,43 @@
 // Function to be returned when Weekly Activities tab is clicked
+
+const weekly_activities = [
+	{
+		id: 1,
+		service: "Sunday Service",
+		day: "Sunday",
+		image: "pic1.jpeg",
+		start_time: "8:00am",
+		end_time: "11:30pm",
+		location: "Assembly Hall",
+	},
+	{
+		id: 2,
+		service: "Prayer Force",
+		day: "Monday",
+		image: "pic1.jpeg",
+		start_time: "7:00pm",
+		end_time: "8:00pm",
+		location: "Casford Field",
+	},
+	{
+		id: 3,
+		service: "Midweek Service",
+		day: "Thursday",
+		image: "pic1.jpeg",
+		start_time: "6:30pm",
+		end_time: "8:00pm",
+		location: "New Life Baptist",
+	},
+];
 exports.weekly_activities = (req, res) => {
 	// res.send(req.session);
 	if (req.session.admin_id == undefined) {
 		res.redirect("/login");
 	} else {
-		res.render("admin/weekly_activities", { page: "weekly_activities" });
+		res.render("admin/weekly_activities", {
+			page: "weekly_activities",
+			activities: weekly_activities,
+		});
 	}
 };
 
